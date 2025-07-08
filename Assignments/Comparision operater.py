a77bf9589971a3c23c338c714d99aa451d934561
SakeKusumalatha <sakekusumalatha@gmail.com>
2025-07-08 10:06:55 +0530

    fourth program

---
+++ Assignments/Comparision operater.py
@@ -0,0 +1,8 @@
+c=26
+d=53
+print('Equal to(c==d):',c==d)#Equal to(c==d): False
+print('Not Equal to(c!=d):',c!=d)#Not Equal to(c!=d): True
+print('Graeter than(c>d):',c>d)#Graeter than(c>d): False
+print('Less than(c,d):',c<d)#Less than(c,d): True
+print('Greater than or Equal to(c>=d):',c>=d)#Greater than or Equal to(c>=d): False
+print('Less than or Equal to(c<=d):',c<=d)#Less than or Equal to(c<=d): True
