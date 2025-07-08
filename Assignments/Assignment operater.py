1cbdc0fd31666f4540c81b3e3405a4948030f5d4
SakeKusumalatha <sakekusumalatha@gmail.com>
2025-07-08 15:39:45 +0530

    fifth program

---
+++ Assignments/Assignment operater.py
@@ -0,0 +1,15 @@
+a=15
+a+=15
+print('Add & Assign(a+=5):',a)#Add & Assign(a+=5): 30
+a-=15
+print('Subtract & Assign(a-=5):',a)#Subtract & Assign(a-=5): 15
+a*=15
+print('Multiply & Assign(a*=5):',a)#Multiply & Assign(a*=5): 225
+a/=15
+print('Devide & Assign(a/=5):',a)#Devide & Assign(a/=5): 15.0
+a//=15
+print('Floor Devide & Assign(a//=5):',a)#Floor Devide & Assign(a//=5): 1.0
+a%=15
+print('Modulus & Assign(a%=5):',a)#Modulus & Assign(a%=5): 1.0
+a**=15
+print('Exponential & Assign(a**=5):',a)#Exponential & Assign(a**=5): 1.0
