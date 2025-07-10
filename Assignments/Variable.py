3baa7a41adae14f413c4e7ac78545f49dce45264
SakeKusumalatha <sakekusumalatha@gmail.com>
2025-07-10 21:08:50 +0530

    Variables

--- Assignments/Variable.py
+++
@@ -1,2 +0,0 @@
-myvar=12
-myvar()
---
+++ Class Work/Variable.py
@@ -0,0 +1,26 @@
+#Basic Variable Assignment
+product_name="Laptop"
+price = 40000
+in_stock = True
+print(product_name,price,in_stock)#True
+
+#Multiple Assignment
+a,b,c = 10,20,30
+print(a,b,c)#10 20 30
+x=y=z=100
+print(x,y,z)#100 100 100
+
+#Reassignment
+x=5
+x=10
+print(x)#10
+
+#Swapping Variable
+a,b=5,10
+a,b=b,a
+print(a,b)#10 5
+
+#Deleting Variables
+x=100
+print(x)#100
+del x
