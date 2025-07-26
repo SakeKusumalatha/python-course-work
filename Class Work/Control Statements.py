5e7d921f2e378e245d7ff1d931db85c31c562065
SakeKusumalatha <sakekusumalatha@gmail.com>
2025-07-26 14:48:31 +0530

    Control Statements

--- Class Work/Control Statements.py
+++ Class Work/Control Statements.py
@@ -67,3 +67,75 @@ for num in range(1,10):
     if num%2==0:
         continue
     print(num) #1,3,5,7,9
+
+#4. While Loop Statements
+#4.1 example
+moves=24
+winningpoint=int(input("Enter the winning point: "))
+while moves>0:
+    if moves==winningpoint:
+        print("Congrats!!! You Won the game")
+        break
+    print(f"{moves} are left.You have chance to win the game")
+    moves-=1
+else:
+    print("Game Over.Try again")
+#OUTPUT:
+#Enter the winning point: 10
+#24 are left.You have chance to win the game
+#23 are left.You have chance to win the game
+#22 are left.You have chance to win the game
+#21 are left.You have chance to win the game
+#20 are left.You have chance to win the game
+#19 are left.You have chance to win the game
+#18 are left.You have chance to win the game
+#17 are left.You have chance to win the game
+#16 are left.You have chance to win the game
+#15 are left.You have chance to win the game
+#14 are left.You have chance to win the game
+#13 are left.You have chance to win the game
+#12 are left.You have chance to win the game
+#11 are left.You have chance to win the game
+#Congrats!!! You Won the game
+
+#4.2 Example:
+s='Python programming'
+ind=0
+n=len(s)
+while ind<n:
+    print(s[ind])
+    ind+=1
+#OUTPUT:
+#P
+#y
+#t
+#h
+#o
+#n
+
+#p
+#r
+#o
+#g
+#r
+#a
+#m
+#m
+#i
+#n
+#g
+
+
+#4.3 Example:
+n=3
+while n<19:
+    print(n)
+    n=n+3
+#OUTPUT:
+#3
+#6
+#9
+#12
+#15
+#18
+
