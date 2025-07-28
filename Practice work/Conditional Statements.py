2798cc1f5c35936cbfb7d8afe6edaacb26160c13
SakeKusumalatha <sakekusumalatha@gmail.com>
2025-07-28 21:26:13 +0530

    Conditional Statements

--- Practice work/Conditional Statements.py
+++ Practice work/Conditional Statements.py
@@ -42,3 +42,105 @@ if marks >= 35:
 else:
     print("Fail")
 
+#7. Check if number is 3-digit
+num = 123
+if 100 <= num <= 999:
+    print("3-digit number")
+else:
+    print("Not a 3-digit number")
+
+#8. Check if Charecter is vowel
+Char = "a"
+if Char.lower() in 'aeiouAeiou':
+    print("vowel")
+else:
+    print("Not a vowel")
+
+#9. Check Greatest of two numbers
+a, b = 7, 9
+if a > b:
+    print(f"{a} is grater")
+else:
+    print(f"{b} is greater")
+
+#10. Check smallest of two numbers
+a, b = 3, 8
+if a < b:
+    print(f"{3} is smallest number")
+else:
+    print(f"{b} is smallest number")
+
+#11. Check if number is zero
+num = 0
+if num == 0:
+    print("number is zero")
+else:
+    print("nubers is not zero")
+
+#12. Check if number is multiple of 10
+num = 50
+if num % 10 == 0:
+    print("Multiple of 10")
+else:
+    print("Not amultiple of 10")
+
+#13. Check if age is eligible to vote(18+)
+age = 19
+if age >= 18:
+    print("eligible for vote")
+else:
+    print("Not eligible for vote")
+
+#14. Check if number is between 1 and 100
+num = 45
+if 1 <= num <= 100:
+    print("in range")
+else:
+    print("Out of range")
+
+#15. check if number is square of another
+a, b = 4, 2
+if a == b ** 2:
+    print(f"{a} is square of {b}")
+else:
+    print(f"{a} is not square of {b}")
+
+#16. Check if two strings are equal
+str1, str2 ="apple","apple"
+if str1 == str2:
+    print("strings are equal")
+else:
+    print("strings are not equal")
+
+#17. Check if a number is prime
+num = 7
+if num >1:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not a prime number")
+            break
+    else:
+        print("prime number")
+else:
+    print("Not a prime number")
+
+#18. Check if number is positive and even
+num = 12
+if num > 0 and num % 2 == 0:
+    print("positive and even number")
+else:
+    print("not a positive and even number")
+
+#19. Check if character is uppercase
+ch = "A"
+if ch.isupper():
+    print("uppercase letter")
+else:
+    print("Not an uppercase letter")
+
+#20. Check if temperature is hot(>30)
+temp = 35
+if temp > 30:
+    print("It's hot")
+else:
+    print("Temperature is normal")
