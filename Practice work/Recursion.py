e02d811215dd489bbc4b543def3b3add836164d7
SakeKusumalatha <sakekusumalatha@gmail.com>
2025-08-10 18:16:48 +0530

    Recursions

---
+++ Practice work/Recursion.py
@@ -0,0 +1,27 @@
+
+#Recursion
+#1 To N
+
+def number(n):
+    if n == 0:
+        return
+    number(n-1)
+    print(n)
+number(5)
+1
+2
+3
+4
+5
+# N To !
+def number(n):
+    if n == 0:
+        return 
+    print(n)
+    number(n-1)
+
+number(4)
+4
+3
+2
+1
