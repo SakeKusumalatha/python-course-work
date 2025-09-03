4914dd321eb8359c57067357a4972055f80936a4
SakeKusumalatha <sakekusumalatha@gmail.com>
2025-09-03 10:50:56 +0530

    Generators

---
+++ Class Work/Generators.py
@@ -0,0 +1,14 @@
+def simple_generators():
+    print("Start")
+    yield 1
+    yield 2
+    yield 3
+    print("End")
+gen = simple_generators()
+print(next(gen))
+print(next(gen))
+print(next(gen))
+'''Start
+1
+2
+3'''
